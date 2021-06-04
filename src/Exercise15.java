7f616a948d4333a0df0a462611202e173575f11e
RobertUriarte <robbyuriarte2@gmail.com>
2021-06-04 00:04:26 -0400

    Initial commit

---
+++ src/Exercise15.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Robert Uriarte
+ */
+import java.util.Scanner;
+public class Exercise15 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.printf("Please enter the password: ");
+        String password = scan.nextLine();
+        if(password.compareTo("Lebron_James") == 0){
+            System.out.print("Password is correct!");
+        }
+
+        else{
+            System.out.print("Wrong password, try again");
+        }
+
+
+    }
+}
