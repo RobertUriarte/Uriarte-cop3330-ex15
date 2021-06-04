600c61b140d500038f84b35fb19e836867c49d0d
RobertUriarte <robbyuriarte2@gmail.com>
2021-06-04 15:20:16 -0400

    Maven

--- src/main/java/Exercise15.java
+++ src/main/java/Exercise15.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Exercise15 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.printf("Please enter the password: ");
+        System.out.print("Please enter the password: ");
         String password = scan.nextLine();
         if(password.compareTo("Lebron_James") == 0){
             System.out.print("Password is correct!");
